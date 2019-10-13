1f36f55ebcad59864fc79214e7dc3f5fd495e38c
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-10-13 13:26:53 +0200

    asfd

--- frontend/src/test/scala/bloop/testing/BaseSuite.scala
+++ frontend/src/test/scala/bloop/testing/BaseSuite.scala
@@ -32,6 +32,7 @@ import monix.eval.Task
 import bloop.io.Paths
 import monix.execution.misc.NonFatal
 import bloop.logging.RecordingLogger
+import bloop.logging.BspServerLogger
 
 class BaseSuite extends TestSuite with BloopHelpers {
   val pprint = _root_.pprint.PPrinter.BlackWhite
@@ -114,6 +115,11 @@ class BaseSuite extends TestSuite with BloopHelpers {
       case NonFatal(t) =>
         obtainedState.state.logger match {
           case logger: RecordingLogger => logger.dump(); throw t
+          case logger: BspServerLogger =>
+            logger.underlying match {
+              case logger: RecordingLogger => logger.dump()
+              case _ => throw t
+            }
           case _ => throw t
         }
     }
